eef0307aef9ce830d6750f399f1594767496d3c2
Thalisson Moura <thalissonmoura138@gmail.com>
2025-04-16 07:54:08 -0300

    :recycle:refactor:remove arquivo que não está sendo usado

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Main } from "./pages/main.tsx";
-import { EditResume } from "./pages/update/index.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/edit" element={<EditResume />} />
       </Routes>
     </BrowserRouter>
   );
